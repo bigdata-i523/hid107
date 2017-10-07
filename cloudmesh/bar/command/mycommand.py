fb3a4f31324caa8232702985e509e0a71f41ca4a
nijuan2012 <31349183+nijuan2012@users.noreply.github.com>
2017-10-07 10:12:32 +0900

    Create mycommand.py

---
+++ cloudmesh/bar/command/mycommand.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from cloudmesh.shell.command import command
+from cloudmesh.shell.command import PluginCommand
+from cloudmesh.bar.api.manager import Manager
+
+class BarCommand(PluginCommand):
+
+    # noinspection PyUnusedLocal
+    @command
+    def do_bar(self, args, arguments):
+        """
+        ::
+          Usage:
+                bar --file=FILE
+                bar list
+          This command does some useful things.
+          Arguments:
+              FILE   a file name
+          Options:
+              -f      specify the file
+        """
+        arguments.FILE = arguments['--file'] or None
+
+        print(arguments)
+
+        m = Manager()
+
+
+        if arguments.FILE:
+            print("option a")
+            m.list(arguments.FILE)
+
+        elif arguments.list:
+            print("option b")
+            m.list("just calling list without parameter")
